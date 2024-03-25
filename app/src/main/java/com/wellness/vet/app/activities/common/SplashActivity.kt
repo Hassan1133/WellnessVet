a9967a055d249a59a54af5456743e6730ef9a0ed
Fakhir Ali <fakhirali.ofc@gmail.com>
2024-03-25 22:26:43 +0500

    Doctor Activities Added

--- app/src/main/java/com/wellness/vet/app/activities/common/SplashActivity.kt
+++ app/src/main/java/com/wellness/vet/app/activities/common/SplashActivity.kt
@@ -10,5 +10,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
