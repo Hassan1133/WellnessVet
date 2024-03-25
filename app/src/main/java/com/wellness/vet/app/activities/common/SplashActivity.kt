1c613a0eb6c31130a0e4f33b68bc5d838ab94ccc
Hassan1133 <mhassanidrees575@gmail.com>
2024-03-25 21:39:28 +0500

    splash screen added

---
+++ app/src/main/java/com/wellness/vet/app/activities/common/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.wellness.vet.app.activities.common
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wellness.vet.app.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/wellness/vet/app/activities/user/UserLoginSignupActivity.kt
@@ -0,0 +1,15 @@
+package com.wellness.vet.app.activities.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.wellness.vet.app.R
+
+class UserLoginSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_login_signup)
+    }
+}
