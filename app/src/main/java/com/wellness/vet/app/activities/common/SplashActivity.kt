5e9bf6083585e28c4876306b8defb0369116a81a
Hassan1133 <mhassanidrees575@gmail.com>
2024-03-25 21:43:05 +0500

    user model added

--- app/src/main/java/com/wellness/vet/app/activities/common/SplashActivity.kt
+++ app/src/main/java/com/wellness/vet/app/activities/common/SplashActivity.kt
@@ -1,9 +1,11 @@
 package com.wellness.vet.app.activities.common
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.wellness.vet.app.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/wellness/vet/app/models/UserModel.kt
@@ -0,0 +1,4 @@
+package com.wellness.vet.app.models
+
+class UserModel {
+}
