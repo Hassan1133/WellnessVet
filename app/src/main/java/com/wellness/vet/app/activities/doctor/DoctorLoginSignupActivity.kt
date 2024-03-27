696e3c3a839c1c9754ad373f7b57d7c30e9e2d00
Fakhir Ali <fakhirali.ofc@gmail.com>
2024-03-27 21:05:18 +0500

    doctor login activity design updated

--- app/src/main/java/com/wellness/vet/app/activities/doctor/DoctorLoginSignupActivity.kt
+++ app/src/main/java/com/wellness/vet/app/activities/doctor/DoctorLoginSignupActivity.kt
@@ -2,10 +2,16 @@ package com.wellness.vet.app.activities.doctor
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.wellness.vet.app.databinding.ActivityDoctorLoginSignupBinding
 
 class DoctorLoginSignupActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDoctorLoginSignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDoctorLoginSignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
