55d0c078395fec8f6e5b56372551a53b8850ab6d
Fakhir Ali <fakhirali.ofc@gmail.com>
2024-03-27 20:32:03 +0500

    Doctor Activities updated

--- app/src/main/java/com/wellness/vet/app/activities/doctor/DoctorDashBoardActivity.kt
+++ app/src/main/java/com/wellness/vet/app/activities/doctor/DoctorDashBoardActivity.kt
@@ -1,11 +1,7 @@
 package com.wellness.vet.app.activities.doctor
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.wellness.vet.app.R
 
 class DoctorDashBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
